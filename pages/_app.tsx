c5e0d7d97b90bcee7ba3fe2f5309ddcc2e72605f
Ajay <ajay.goswami@coderower.com>
2025-11-16 18:33:33 +0530

    feat: add Vercel Analytics and Speed Insights for performance tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
-import { ThemeProvider } from '../contexts/ThemeContext'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { ThemeProvider } from "../contexts/ThemeContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
-  )
+  );
 }
